d5bf6087d713664a0487a71d8345b68b41c2d745
aramydEEE <aramydeee@gmail.com>
2025-05-06 13:58:55 +0100

    included aside element

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const buttonClick = document.getElementById("info");
-buttonClick.addEventListener('click',myFunction);
-const fullName =[];
+buttonClick.addEventListener('click', myFunction);
+let fullName ="";
 function myFunction(e) {
     e.preventDefault()
     const first = document.querySelector('#fname').value
     const second = document.querySelector('#sname').value
-    fullName.push(first,second)
-    console.log(first)
+    fullName =`${first} ${second}`
+    console.log(fullName)
+    document.getElementById("viewer").textContent = fullName;
+    
 }
-console.log(fullName);
+// const word = document.getElementById("word")
 
+
+
+// document.addEventListener('click', (e)=>{
+//     e.preventDefault()
+//     if( e.target.id === "info"){
+//         console.log("it is working")
+//     }
+//     else if(e.target.id === "redirect"){
+        
+//         console.log(document.getElementById('redirect').getAttribute("target","_blank") ) 
+//         // word.style.display = "block"
+        
+//     //     setTimeout(() => {
+//     //          word.style.display = "none"
+//     //     }, 2000);
+//      }
+    
+// })
+
+// // const div = document.createElement("div")
